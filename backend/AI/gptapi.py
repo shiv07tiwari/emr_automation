053f8248990328d66940c9795b2def2cc180c48a
Shivansh <stiwari@instawork.com>
2024-01-08 13:59:12 +0530

    remove UI code

--- backend/AI/gptapi.py
+++ backend/AI/gptapi.py
@@ -1,11 +1,9 @@
-import openai
-
 from openai import OpenAI
 
 
 def getGPTClient():
     client = OpenAI(
-        api_key="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",
+        api_key="key",
     )
 
     return client
